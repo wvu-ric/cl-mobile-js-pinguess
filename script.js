48f5b18c97df2c0f0590564722556c85651316e2
Nancy Andrews <nancy@nancyandrews.com>
2016-02-18 15:27:55 -0500

    Completed assignment

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-var pinCode = "9999";
-
 // leftPad by timrwood
 // http://stackoverflow.com/questions/8043026/javascript-format-number-to-have-2-digit
+// completed with lots of help
+
+var pinCode = "0029";
+
 function leftPad(number, targetLength) {
-    var output = number + '';
+    var output = number + "";
     while (output.length < targetLength) {
         output = '0' + output;
     }
     return output;
-}
-
-function check(combo){
-	
-}
+  }
 
 var found = false;
 var count = 0;
 while(!found && count < 10000){
-	
+  var pinCode4 = leftPad(count,4);
+  count++;
+  if (pinCode4 == pinCode){
+    found = true;
+  }
 }
-
 if (found) {
-	alert();
+	alert("The pincode is" + " " + pinCode);
 }
